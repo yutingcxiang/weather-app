4b419f37aead0084cfc3dabd1c91542a1861806a
yutingcxiang <yutingcxiang@gmail.com>
2019-01-08 18:55:42 -0500

    Move API_KEY to dotenv

--- server.js
+++ server.js
@@ -5,13 +5,15 @@ const request = require('request');
 const app = express();
 const port = process.env.PORT || 5000;
 
+require('dotenv').config()
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.post('/weather', function(req, res){
-  let apiKey = 'f16eed3c3855a58355e9745081ed7e4c';
+  let apiKey = process.env.REACT_APP_API_KEY;
   let units = 'imperial';
   let city = req.body.input;
   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}&units=${units}`
