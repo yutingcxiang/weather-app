8ebab04eb22ab0fbdb8c9130f2836b13329990f8
yutingcxiang <yutingcxiang@gmail.com>
2019-01-07 19:34:01 -0500

    Create WeatherContainer and Weather components

--- client/src/App.js
+++ client/src/App.js
@@ -1,56 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import WeatherContainer from './containers/WeatherContainer.js';
+// import Moment from 'react-moment';
 
 class App extends Component {
-  state = {
-    response: '',
-    post: '',
-    responseToPost: '',
-  };
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
-  }
-
-  callApi = async () => {
-    const response = await fetch('/api/weather');
-    const body = await response.json();
-    if (response.status !== 200) throw Error(body.message);
-    return body;
-  };
-
-  handleSubmit = async e => {
-    e.preventDefault();
-    const response = await fetch('/api/location', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ post: this.state.post }),
-    });
-    const body = await response.text();
-    this.setState({ responseToPost: body });
-  };
 
 render() {
     return (
       <div className="App">
-        <header className="App-header">
-        </header>
-        <p>{this.state.response}</p>
-        <form onSubmit={this.handleSubmit}>
-          <p>
-            <strong>Post to Server:</strong>
-          </p>
-          <input
-            type="text"
-            value={this.state.post}
-            onChange={e => this.setState({ post: e.target.value })}
-          />
-          <button type="submit">Submit</button>
-        </form>
-        <p>{this.state.responseToPost}</p>
+        <WeatherContainer />
       </div>
     );
   }
---
+++ client/src/components/Weather.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Weather= (props) => {
+  return(
+    <div>
+      <p>Weather here!</p>
+    </div>
+  )
+}
+
+export default Weather;
