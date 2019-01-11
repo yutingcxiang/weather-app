075b4a6a1e1ae0d563ae5e9fa5fc7f247ce10f05
yutingcxiang <yutingcxiang@gmail.com>
2019-01-10 22:54:04 -0500

    Run build

---
+++ client/build/precache-manifest.6feca4ff6ab545b813497020d56e23f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1029ae9bce89f559ad64",
+    "url": "/static/css/main.a0ccc101.chunk.css"
+  },
+  {
+    "revision": "1029ae9bce89f559ad64",
+    "url": "/static/js/main.1029ae9b.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "52653e8032651c8e39f01dd57cca7ab1",
+    "url": "/static/media/city.52653e80.png"
+  },
+  {
+    "revision": "ce6dbd550d1bb5038be8adff43af9e31",
+    "url": "/index.html"
+  }
+];
