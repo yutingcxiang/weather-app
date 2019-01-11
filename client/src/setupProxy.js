b4cca479b9cd01417f5d4934845c259cee15ed58
yutingcxiang <yutingcxiang@gmail.com>
2019-01-11 12:23:11 -0500

    Update Proxy info

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,5 +1,5 @@
 const proxy = require("http-proxy-middleware");
 
 module.exports = app => {
-  app.use(proxy("/weather/*", { target: "http://localhost:5000/" }));
+  app.use(proxy("/weather", { target: "http://localhost:5000", changeOrigin: true}));
 };
