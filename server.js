40245af1dba1f480671c619b2bbceae3006510ec
yutingcxiang <yutingcxiang@gmail.com>
2019-01-16 15:34:53 -0500

    Update scripts

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.get('/', function (req, res) {
 app.use(express.static(path.join(__dirname, 'client/build')))
 
 app.get('*', (request, response) => {
-
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
